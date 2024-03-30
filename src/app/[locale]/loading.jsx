f041d6605c10c6cd014d1e38a6d4da9130f55a6c
salahelfatimi <salahfatimi16@gmail.com>
2024-03-30 19:24:39 +0000

    add loading page style

--- src/app/[locale]/loading.jsx
+++ src/app/[locale]/loading.jsx
@@ -1,17 +1,16 @@
 export default function Loading() {
-    
-    return (
-      <>
-        <div
+  return (
+    <>
+      <div
         className={`fixed  inset-0 flex items-center justify-center bg-[#f4f7fc]  duration-1000 transition z-50 `}
       >
-        <span className={`font-extrabold md:text-5xl text-4xl animate-bounce flex justify-center  `}>
-        <span className=" flex items-center text-[#0149a6] ">
-          
-          Explorem
+        <span
+          className={`font-extrabold text-center items-center gap-1 md:text-5xl text-4xl animate-bounce flex justify-center  `}
+        >
+          Welcome to
+          <span className=" flex items-center text-[#0149a6] ">Explorem</span>
         </span>
-           </span>
       </div>
-      </>
-    )
-  }
+    </>
+  );
+}
