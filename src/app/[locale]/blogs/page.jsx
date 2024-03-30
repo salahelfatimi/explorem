6b160dd38594ac1223fbccc2aa2d854b7e95efea
salahelfatimi <salahfatimi16@gmail.com>
2024-03-30 18:15:49 +0000

    add some change in tradition

--- src/app/[locale]/blogs/page.jsx
+++ src/app/[locale]/blogs/page.jsx
@@ -1,25 +1,36 @@
+import { fetchBlogs } from "@/app/api/data/actions";
+import BlogItem from "@/components/BlogItem";
+import Image from "next/image";
 
-
-import { fetchBlogs } from "@/app/api/data/actions"
-import BlogItem from "@/components/BlogItem"
-
-
-const Blog= async ()=>{
-    const blogs=await fetchBlogs()
-    return(
-        <>
-            
-            <h1> Blog </h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-5 mb-5 px-4 py-5">
-                {blogs?.length > 0 && blogs.map((blog) => (
-
-                    <BlogItem key={blog?.id} blog={blog} />
-
-                ))}
-            </div>
-           
-
-        </>
-    )
-}
-export default Blog
+const Blog = async () => {
+  const blogs = await fetchBlogs();
+  return (
+    <main className="max-w-[52rem] mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 lg:max-w-6xl">
+      <header className=" py-16 sm:text-center">
+        <h1 className=" mb-4 text-3xl sm:text-4xl tracking-tight text-[#134ba1] font-extrabold ">
+          Latest Blog{" "}
+        </h1>
+        <p className="text-lg text-black ">
+          All the latest Explorem news, straight from the team.
+        </p>
+      </header>
+      <div className=" relative">
+        <Image
+          width={1920}
+          height={1000}
+          className="w-full object-cover brightness-50 rounded h-[30rem] bg-no-repeat "
+          src="/image/aboutUs/slideLeft/image2.jpg"
+        />
+        <div className=" absolute bottom-5 gap-2 left-8 flex flex-col">
+          <h2 className=" font-bold text-2xl text-white">
+            23 nurse-scholars received their visa !
+          </h2>
+          <span className="text-[#fff] font-semibold">March 19, 2024</span>
+          <span className="bg-[#134ba1] py-1 px-2 rounded font-bold text-white w-fit">Read more</span>
+          
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Blog;
--- src/app/[locale]/loading.jsx
+++ src/app/[locale]/loading.jsx
@@ -1,4 +1,17 @@
 export default function Loading() {
     
-    return <p>Loading...</p>
+    return (
+      <>
+        <div
+        className={`fixed  inset-0 flex items-center justify-center bg-[#f4f7fc]  duration-1000 transition z-50 `}
+      >
+        <span className={`font-extrabold md:text-5xl text-4xl animate-bounce flex justify-center  `}>
+        <span className=" flex items-center text-[#0149a6] ">
+          
+          Explorem
+        </span>
+           </span>
+      </div>
+      </>
+    )
   }
