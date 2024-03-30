8e3fcd0a490ba467d05531aa2887e66cbca39278
salahelfatimi <salahfatimi16@gmail.com>
2024-03-30 19:26:18 +0000

    add

--- src/app/[locale]/loading.jsx
+++ src/app/[locale]/loading.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
         className={`fixed  inset-0 flex items-center justify-center bg-[#f4f7fc]  duration-1000 transition z-50 `}
       >
         <span
-          className={`font-extrabold text-center items-center gap-1 md:text-5xl text-4xl animate-bounce flex justify-center  `}
+          className={`font-extrabold text-center items-center gap-1 md:text-5xl text-4xl animate-bounce flex flex-col lg:flex-row justify-center  `}
         >
           Welcome to
           <span className=" flex items-center text-[#0149a6] ">Explorem</span>
