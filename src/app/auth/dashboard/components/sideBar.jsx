ce064878a3c36f5b3f1a89f3f13bb89020970088
salahelfatimi <salahfatimi16@gmail.com>
2024-03-31 19:45:20 +0000

    add

--- src/app/auth/dashboard/components/sideBar.jsx
+++ src/app/auth/dashboard/components/sideBar.jsx
@@ -11,9 +11,9 @@ export default function SideBar() {
         <div>
           <h1 className=" font-bold text-[#0149a6] text-xl">Explorem Admin</h1>
         </div>
-        <Link href="/auth/dashboard"  className={` ${path=="/auth/dashboard"?'bg-[#0149a6] text-white ':'text-[#0149a6]'} text-lg text-center font-semibold w-full py-4`}>All Blog</Link>
-        <Link href="/auth/dashboard/addBlog"  className={` ${path=="/auth/dashboard/addBlog"?'bg-[#0149a6] text-white ':'text-[#0149a6]'} text-lg text-center font-semibold w-full py-4`}>Add Blog</Link>
-        <Link href={"#"} className=" text-[#0149a6] text-center  text-lg font-semibold w-full py-4">Cumment</Link>
+        <Link href="/auth/dashboard"  className={` ${path=="/auth/dashboard"?'bg-[#0149a6] duration-700 transition text-white ':'text-[#0149a6]'} text-lg text-center font-semibold w-full py-4`}>All Blog</Link>
+        <Link href="/auth/dashboard/addBlog"  className={` ${path=="/auth/dashboard/addBlog"?'bg-[#0149a6] duration-700 transition text-white ':'text-[#0149a6]'} text-lg text-center font-semibold w-full py-4`}>Add Blog</Link>
+        <Link href={"#"} className=" text-[#0149a6] text-center  text-lg font-semibold duration-700 transition w-full py-4">Cumment</Link>
       </div>
       <div className="flex text-center ">
         <Link href={"/"} className=" bg-red-600 text-white font-bold w-full py-3  ">logOut</Link>
--- src/app/auth/layout.js
+++ src/app/auth/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
       <html>
-        <body>
+        <body className="bg-[#f5f5f5]">
           {/* Layout UI */}
           <main>{children}</main>
         </body>
