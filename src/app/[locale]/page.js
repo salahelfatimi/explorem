ee723c9034977af9ff91472694075f23be25f032
salahelfatimi <salahfatimi16@gmail.com>
2024-04-01 01:45:57 +0000

    add index page

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 import InfiniteHorizontalScroll from "@/components/infiniteHorizontalScroll";
 import TestimonialsSlider from "@/components/testimonialsSlider";
-import {createSharedPathnamesNavigation} from 'next-intl/navigation';
+import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
-const locales = ['en', 'de','ar'] ;
-const {Link} = createSharedPathnamesNavigation({locales});
+const locales = ["en", "de", "ar"];
+const { Link } = createSharedPathnamesNavigation({ locales });
 
 export default function Index() {
   const t = useTranslations("Index");
@@ -21,48 +21,9 @@ export default function Index() {
     { src: "/image/aboutUS/slideLeft/image8.jpg" },
     { src: "/image/aboutUS/slideLeft/image9.jpg" },
   ];
-  const grids = [
-    {
-      id: 1,
-      title: t("grids.0.title"),
-      description: t("grids.0.description"),
-      svg: "training.svg",
-    },
-    {
-      id: 2,
-      title: t("grids.1.title"),
-      description: t("grids.1.description"),
-      svg: "search-job.svg",
-    },
-    {
-      id: 3,
-      title: t("grids.2.title"),
-      description: t("grids.2.description"),
-      svg: "social-management.svg",
-    },
-    {
-      id: 4,
-      title: t("grids.3.title"),
-      description: t("grids.3.description"),
-      svg: "diploma.svg",
-    },
-    {
-      id: 5,
-      title: t("grids.4.title"),
-      description: t("grids.4.description"),
-      svg: "academy-cap.svg",
-    },
-    {
-      id: 6,
-      title: t("grids.5.title"),
-      description: t("grids.5.description"),
-      svg: "passport.svg",
-    },
-  ];
 
   return (
     <>
-
       <div className="bg-[#F5F5F5]  space-y-8 py-12">
         <div className=" flex flex-col items-center justify-center gap-4">
           <h1 className="flex items-center gap-4 justify-center flex-col text-xl lg:text-2xl font-extrabold capitalize">
@@ -105,30 +66,120 @@ export default function Index() {
         </div>
         <div>
           <div className=" container grid items-center gap-4  grid-cols-1  lg:grid-cols-2">
-            {grids.map((ele, index) => (
-              <div
-                key={index}
-                className="flex   bg-white py-2 px-4  shadow rounded-md items-center gap-2"
-              >
-                <div className=" border-[#0149a6]   border-4 p-3 rounded-full">
-                  <Image
-                    width={500}
-                    height={500}
-                    src={`image/index/icon/${ele.svg}`}
-                    className="w-11 min-w-11 fill-white"
-                    alt="explorem"
-                  />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <span className=" text-[#134BA1] font-bold lg:text-xl break-words">
-                    {ele.title}
-                  </span>
-                  <span className=" text-md font-medium">
-                    {ele.description}
-                  </span>
-                </div>
+            <div className="flex   bg-white py-2 px-4  shadow rounded-md items-center gap-2">
+              <div className=" border-[#0149a6]   border-4 p-3 rounded-full">
+                <Image
+                  width={500}
+                  height={500}
+                  src={`image/index/icon/training.svg`}
+                  className="w-11 min-w-11 fill-white"
+                  alt="explorem"
+                />
               </div>
-            ))}
+              <div className="flex flex-col gap-2">
+                <span className=" text-[#134BA1] font-bold lg:text-xl break-words">
+                  {t("grids.0.title")}
+                </span>
+                <span className=" text-md font-medium">
+                  {t("grids.0.description")}
+                </span>
+              </div>
+            </div>
+            <div className="flex   bg-white py-2 px-4  shadow rounded-md items-center gap-2">
+              <div className=" border-[#0149a6]   border-4 p-3 rounded-full">
+                <Image
+                  width={500}
+                  height={500}
+                  src={`image/index/icon/search-job.svg`}
+                  className="w-11 min-w-11 fill-white"
+                  alt="explorem"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className=" text-[#134BA1] font-bold lg:text-xl break-words">
+                  {t("grids.1.title")}
+                </span>
+                <span className=" text-md font-medium">
+                  {t("grids.1.description")}
+                </span>
+              </div>
+            </div>
+            <div className="flex   bg-white py-2 px-4  shadow rounded-md items-center gap-2">
+              <div className=" border-[#0149a6]   border-4 p-3 rounded-full">
+                <Image
+                  width={500}
+                  height={500}
+                  src={`image/index/icon/diploma.svg`}
+                  className="w-11 min-w-11 fill-white"
+                  alt="explorem"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className=" text-[#134BA1] font-bold lg:text-xl break-words">
+                  {t("grids.2.title")}
+                </span>
+                <span className=" text-md font-medium">
+                  {t("grids.2.description")}
+                </span>
+              </div>
+            </div>
+            <div className="flex   bg-white py-2 px-4  shadow rounded-md items-center gap-2">
+              <div className=" border-[#0149a6]   border-4 p-3 rounded-full">
+                <Image
+                  width={500}
+                  height={500}
+                  src={`image/index/icon/diploma.svg`}
+                  className="w-11 min-w-11 fill-white"
+                  alt="explorem"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className=" text-[#134BA1] font-bold lg:text-xl break-words">
+                  {t("grids.3.title")}
+                </span>
+                <span className=" text-md font-medium">
+                  {t("grids.3.description")}
+                </span>
+              </div>
+            </div>
+            <div className="flex   bg-white py-2 px-4  shadow rounded-md items-center gap-2">
+              <div className=" border-[#0149a6]   border-4 p-3 rounded-full">
+                <Image
+                  width={500}
+                  height={500}
+                  src={`image/index/icon/academy-cap.svg`}
+                  className="w-11 min-w-11 fill-white"
+                  alt="explorem"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className=" text-[#134BA1] font-bold lg:text-xl break-words">
+                  {t("grids.4.title")}
+                </span>
+                <span className=" text-md font-medium">
+                  {t("grids.4.description")}
+                </span>
+              </div>
+            </div>
+            <div className="flex   bg-white py-2 px-4  shadow rounded-md items-center gap-2">
+              <div className=" border-[#0149a6]   border-4 p-3 rounded-full">
+                <Image
+                  width={500}
+                  height={500}
+                  src={`image/index/icon/passport.svg`}
+                  className="w-11 min-w-11 fill-white"
+                  alt="explorem"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className=" text-[#134BA1] font-bold lg:text-xl break-words">
+                  {t("grids.5.title")}
+                </span>
+                <span className=" text-md font-medium">
+                  {t("grids.5.description")}
+                </span>
+              </div>
+            </div>
           </div>
         </div>
         <div className=" flex flex-col gap-4">
