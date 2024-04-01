500c9c9af64c75d7242ac817787ab5d6513cee7b
salahelfatimi <salahfatimi16@gmail.com>
2024-04-01 01:15:33 +0000

    add not found

---
+++ src/app/admin/loading.jsx
@@ -0,0 +1,17 @@
+import "./globals.css"
+export default function Loading() {
+  return (
+    <>
+      <div
+        className={`fixed  inset-0 flex items-center justify-center bg-[#f4f7fc]  duration-1000 transition z-50 `}
+      >
+        <span
+          className={`font-extrabold text-center items-center gap-3 md:text-5xl text-4xl animate-bounce flex flex-col lg:flex-row justify-center  `}
+        >
+          Welcome to
+          <span className=" flex items-center text-[#0149a6] ">Explorem</span>
+        </span>
+      </div>
+    </>
+  );
+}
--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { useRouter } from 'next/navigation'
-
+import "./globals.css"
 
 export default function Login() {
   const router = useRouter();
