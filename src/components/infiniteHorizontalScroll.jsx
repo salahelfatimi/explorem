4e1bdfb7dca737146c31bc61c69bc45f7588056c
salahelfatimi <salahfatimi16@gmail.com>
2024-04-07 03:30:47 +0000

    add scrool

--- src/components/infiniteHorizontalScroll.jsx
+++ src/components/infiniteHorizontalScroll.jsx
@@ -1,13 +1,17 @@
 "use client";
-
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
 
-const InfiniteHorizontalScroll = ({ images }) => {
+export default  function EmblaCarousel({ images }) {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+
   return (
-    <div className="flex overflow-hidden space-x-4   ">
-      <div className="flex space-x-4 animate-loop-scroll">
+    <div className=" overflow-hidden" ref={emblaRef}>
+      <div className="flex flex-row gap-6">
         {images?.map((ele, index) => (
-          <div key={index}>
+          <div className=" space-x-4" key={index}>
             <Image
               blurDataURL={ele.src}
               placeholder="blur"
@@ -22,25 +26,6 @@ const InfiniteHorizontalScroll = ({ images }) => {
           </div>
         ))}
       </div>
-      <div className="flex space-x-4 animate-loop-scroll" aria-hidden="true">
-        {images?.map((ele, index) => (
-          <div key={index}>
-            <Image
-              blurDataURL={ele.src}
-              placeholder="blur"
-              loading="lazy"
-              quality={100}
-              width={800}
-              height={550}
-              src={ele.src}
-              className="max-w-none rounded-sm h-52 object-cover w-auto"
-              alt="explorem"
-            />
-          </div>
-        ))}
-      </div>
     </div>
   );
-};
-
-export default InfiniteHorizontalScroll;
+}
