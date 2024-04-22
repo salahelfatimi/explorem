b7bcb2a1baf69fa7e48518d4a8318255e7d70a12
salahelfatimi <salahfatimi@gmail.com>
2024-04-22 01:20:57 +0100

    remove from slide

--- src/components/fetchImageScroll.jsx
+++ src/components/fetchImageScroll.jsx
@@ -1,16 +1,20 @@
 import { fetchImageScroll } from "@/app/api/data/ImageScroll/actions";
-import GetBase64Image from "@/app/base64/getBase64ImageSingle";
-
+import Image from "next/image";
 export default async function FetchImageScroll() {
   const images = await fetchImageScroll();
   return (
     <>
       {images?.map((src, index) => (
         <div className=" space-x-4 bg-white p-2" key={index}>
-          <GetBase64Image
-            imageUrl={src.imageUrl}
-            alt={src.title}
-            className={"max-w-none   rounded-sm h-52 w-auto"}
+          <Image
+            blurDataURL={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAHCAIAAAC+zks0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAA5ElEQVR4nAHZACb/AMzN3dnS4N3d8tvV7dnj+9igosc+KbdENdatsf/w+wCqmJzh4PLf1eLl3e/G0ejKoqXXfUq8YUfes7X/+f8Aqo2UjniHuaO9l4WWmZCaxJqctGNJlFRHn3p60ainAGVdbSEjPpCY1xEbQRAKCIw4OWM3QRoGEkssKy8XFAB5aHdMSGJydaI1M1MMAgBIDRxiOD9UP0pMNkUeBwcAlYqeGRIcFRIbR0NTX152FhgvRTpGe2yKrpWzelA6AIaJqDMwOUdJVl5bcJebtE5QaTkyQ76hstK0zsuSfGgmZX5k3blAAAAAAElFTkSuQmCC`}
+            placeholder="blur"
+            quality={100}
+            width={800}
+            height={550}
+            src={src.imageUrl}
+            className="max-w-none   rounded-sm h-52 w-auto"
+            alt="explorem"
           />
         </div>
       ))}
