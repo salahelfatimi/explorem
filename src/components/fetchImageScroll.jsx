c3e6e2fc4c6e56254a44159daa6769e394a9885d
salahelfatimi <salahfatimi@gmail.com>
2024-05-09 21:56:53 +0100

    add Explorem SARL

--- src/components/fetchImageScroll.jsx
+++ src/components/fetchImageScroll.jsx
@@ -59,7 +59,7 @@ export default async function FetchImageScroll() {
       {images?.map((src, index) => (
         <div className=" space-x-4 bg-white p-2" key={index}>
           <Image
-            title="Explorem"
+            title="Explorem SARL"
             blurDataURL={`${src.base64}`}
             placeholder="blur"
             quality={50}
@@ -67,7 +67,7 @@ export default async function FetchImageScroll() {
             height={src.height}
             src={`/image/slide/${src.imageUrl}`}
             className="max-w-none min-w-80   rounded-sm h-52 w-auto"
-            alt="Explorem "
+            alt="Explorem SARL"
           />
         </div>
       ))}
