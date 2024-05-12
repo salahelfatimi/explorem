b84bd2410c543e9d6e787a83231c848b166342ea
salahelfatimi <salahfatimi@gmail.com>
2024-05-12 13:29:21 +0100

    add

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -12,7 +12,8 @@ export async function generateMetadata() {
   return {
     title: { default: "Explorem SARL", template: "%s - Explorem SARL" },
     description: "We Make A Difference, We Make It Happen.",
-    metadataBase: new URL('https://explorem.net'),
+    url: 'https://explorem.net/',
+    metadataBase: new URL('https://explorem.net/'),
     alternates: {
       canonical: "/en",
       languages: {
