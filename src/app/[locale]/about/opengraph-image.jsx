c15ff3bbe1f930c967de8d60635d54d525aa3375
salahelfatimi <salahfatimi@gmail.com>
2024-05-12 14:19:15 +0100

    add

--- src/app/[locale]/about/opengraph-image.jsx
+++ src/app/[locale]/about/opengraph-image.jsx
@@ -1,8 +1,6 @@
 import { ImageResponse } from '@vercel/og';
 
-export const config = {
-  runtime: 'experimental-edge',
-};
+export const runtime = "experimental-edge"
 
 export default function () {
   return new ImageResponse(
