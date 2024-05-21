1045265bb67a8dd0f67b208897145c243cea8cbc
salahelfatimi <salahfatimi@gmail.com>
2024-05-21 01:11:09 +0100

    add

--- src/app/[locale]/blogs/[title]/page.jsx
+++ src/app/[locale]/blogs/[title]/page.jsx
@@ -30,6 +30,7 @@ export async  function generateMetadata({ params: { title } }, parent) {
       images: [
         {
           url: [`/image/blogExplorem/${post.Org}`, ...previousImages],
+          blurDataURL: post.base64,
           width: 1200,
           height: 630,
           alt: post.title,
