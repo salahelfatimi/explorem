ea8c593e589404ea86ce59cebfb920f89b1c99e0
salahelfatimi <salahfatimi@gmail.com>
2024-05-23 20:12:04 +0100

    add

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -2,19 +2,32 @@ import { NextIntlClientProvider, useMessages } from "next-intl";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
-import { GoogleAnalytics } from '@next/third-parties/google'
 import { getMessages} from "next-intl/server";
 import { Suspense } from "react";
 import Loading from "./loading";
+import Script from "next/script";
 export default async  function LocaleLayout({ children, params: { locale } }) {
   const messages = await getMessages();
   
 
   return (
     <html  lang={locale} className=" !scroll-smooth">
+      
       <head>
         <meta property="og:url" content={`https://explorem.net`}/>
-        
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id='google-analytics' strategy='lazyOnload'>
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
       </head>
      
       <body className="bg-[#f5f5f5]  scrollbar scrollbar-thumb-[#134ba1]  " >
@@ -26,7 +39,7 @@ export default async  function LocaleLayout({ children, params: { locale } }) {
           </main>
         </NextIntlClientProvider>
       </body>
-      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_MEASUREMENT_ID} />
+     
      
     </html>
   );
