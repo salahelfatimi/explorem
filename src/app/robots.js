5738523ce640eaf9be98d8778d3f57f60e7addea
salahelfatimi <salahfatimi@gmail.com>
2024-06-24 15:59:18 +0100

    add

--- src/app/robots.js
+++ src/app/robots.js
@@ -2,7 +2,7 @@ export default function robots() {
   return {
     rules: {
       userAgent: '*',
-      allow: ['/en'],
+      allow: '/',
       
     },
     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
