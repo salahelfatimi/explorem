b9f10254bc1fb4475a15219997c818ac659c692f
salahelfatimi <salahfatimi@gmail.com>
2024-06-24 16:04:48 +0100

    add

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,6 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/en',
-      
-    },
+   
     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
   }
 }
